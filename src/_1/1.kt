7243d08d3fa591843a569ca0e11073dfec555c49
Simon <simon.blom@spadn.se>
2019-12-01 22:00:56 +0100

    Update 1.kt
    Cleared 1

--- src/_1/1.kt
+++ src/_1/1.kt
@@ -2,9 +2,29 @@ package _1
 
 import java.io.File
 
-fun main(args: Array<String>) {
-    val lineList = mutableListOf<String>()
+fun calculateFuelRequirement(mass: Double): Double {
+    return kotlin.math.floor(mass / 3) - 2
+}
 
-    File("src/_1/in").useLines { lines -> lines.forEach { lineList.add(it) }}
-    lineList.forEach { println(">  " + it) }
+fun main(args: Array<String>) {
+    val path = System.getProperty("user.dir")
+    val lineList = mutableListOf<String>()
+    File(path + "/src/_1/in").useLines { lines -> lines.forEach { lineList.add(it) }}
+
+    var sum : Double = 0.0
+    for(line in lineList) {
+        sum += calculateFuelRequirement(line.toDouble())
+    }
+    println("Part 1: "+sum.toInt())
+
+    sum = 0.0
+    var fuelRequirement: Double
+    for(line in lineList) {
+        fuelRequirement = calculateFuelRequirement(line.toDouble())
+        while(fuelRequirement > 0.0) {
+            sum += fuelRequirement
+            fuelRequirement = calculateFuelRequirement(fuelRequirement)
+        }
+    }
+    println("Part 2: "+sum.toInt())
 }
